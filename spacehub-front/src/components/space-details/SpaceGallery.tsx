a610c638963b57d550d680354c2c6b2ef7f37179
Isaias <99995191+isaias30silva@users.noreply.github.com>
2026-07-13 15:42:36 -0300

    feat(search-details): modificação no layout da galeria de imagens da tela de detalhes

--- spacehub-front/src/components/space-details/SpaceGallery.tsx
+++ spacehub-front/src/components/space-details/SpaceGallery.tsx
@@ -1,11 +1,31 @@
 export default function SpaceGallery() {
   return (
-    <div
+    <section
       className="
-        h-[400px]
-        rounded-2xl
-        bg-slate-200
+        grid
+        grid-cols-1
+        md:grid-cols-4
+        gap-3
       "
-    />
+    >
+      <div
+        className="
+          md:col-span-2
+          h-[400px]
+          rounded-xl
+          bg-slate-300
+        "
+      />
+
+      <div className="space-y-3">
+        <div className="h-[193px] rounded-xl bg-slate-200" />
+        <div className="h-[193px] rounded-xl bg-slate-200" />
+      </div>
+
+      <div className="space-y-3">
+        <div className="h-[193px] rounded-xl bg-slate-200" />
+        <div className="h-[193px] rounded-xl bg-slate-200" />
+      </div>
+    </section>
   );
 }
