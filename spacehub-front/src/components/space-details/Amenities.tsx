0986df1796be9ed01d47875eb79c750df6c64aed
Isaias <99995191+isaias30silva@users.noreply.github.com>
2026-07-13 15:45:25 -0300

    feat(search-details): adicionando icones aos itens de cortesia (amenities) da tela de detalhes

--- spacehub-front/src/components/space-details/Amenities.tsx
+++ spacehub-front/src/components/space-details/Amenities.tsx
@@ -1,30 +1,67 @@
+import { Wifi, Car, Monitor, Snowflake } from "lucide-react";
+
 export default function Amenities() {
-  const amenities = ["Wi-Fi", "Projetor", "Ar condicionado", "Estacionamento"];
+  const amenities = [
+    {
+      icon: Wifi,
+      label: "Wi-Fi",
+    },
+    {
+      icon: Monitor,
+      label: "Projetor",
+    },
+    {
+      icon: Snowflake,
+      label: "Ar condicionado",
+    },
+    {
+      icon: Car,
+      label: "Estacionamento",
+    },
+  ];
 
   return (
     <section className="mt-10">
-      <h2 className="font-semibold text-xl">Comodidades</h2>
+      <h2
+        className="
+          text-xl
+          font-semibold
+        "
+      >
+        Comodidades
+      </h2>
 
       <div
         className="
           grid
-          grid-cols-2
+          grid-cols-1
+          sm:grid-cols-2
           gap-4
-          mt-4
+          mt-5
         "
       >
-        {amenities.map((item) => (
-          <div
-            key={item}
-            className="
-              border
-              rounded-lg
-              p-3
-            "
-          >
-            {item}
-          </div>
-        ))}
+        {amenities.map((item) => {
+          const Icon = item.icon;
+
+          return (
+            <div
+              key={item.label}
+              className="
+                border
+                rounded-xl
+                p-4
+
+                flex
+                items-center
+                gap-3
+              "
+            >
+              <Icon size={20} />
+
+              {item.label}
+            </div>
+          );
+        })}
       </div>
     </section>
   );
