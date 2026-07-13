16e68c1d06e4a2871a0890f1c591ce524d8376b9
Isaias <99995191+isaias30silva@users.noreply.github.com>
2026-07-13 02:19:31 -0300

    feat(home): criacao da secao principal da home page

--- spacehub-front/src/app/page.tsx
+++ spacehub-front/src/app/page.tsx
@@ -1,16 +1,17 @@
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
+import Hero from "@/components/home/Hero";
 
 export default function Home() {
   return (
     <>
       <Navbar />
 
-      <main>Home Page SpaceHub
-        
+      <main>
+        <Hero />
       </main>
 
-    <Footer />
+      <Footer />
     </>
   );
 }
